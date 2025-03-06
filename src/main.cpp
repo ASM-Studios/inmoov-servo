58e12eaf5038e6f26f3a6b11d3b8d2b18056cc5e
m-brl <mathieu.borel@epitech.eu>
2025-03-06 16:18:57 +0100

    wip(wip): wip
    Signed-off-by: m-brl <mathieu.borel@epitech.eu>

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,17 @@
 #include <pico/stdlib.h>
 #include <hardware/pwm.h>
 #include "Servo.hpp"
+#include <vector>
 
 int main() {
     stdio_init_all();
 
     int angle = 0;
     bool isRunning = true;
-    control::Servo servo({0, 180}, 0);
 
     while (isRunning) {
-        scanf("%d", &angle);
-        servo.setAngle(angle);
+        printf("Hello world\n");
         sleep_ms(1000);
+
     }
 }
